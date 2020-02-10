1d981fdb3a0b7cbed19ef457947af76ca5aa79c8
tdj03001 <tyjenki@gmail.com>
2020-02-10 15:52:55 -0500

    added some dev notes to keep me on track

--- data/noteData.js
+++ data/noteData.js
@@ -6,3 +6,8 @@ const noteArray = [
 ]
 
 module.exports = noteArray;
+
+// bring db.json back and have just the array  [{"title":"Test Title","text":"Test text"}]
+
+// deploy to heroku, need index.php and something else (composer.json?)
+//deployt to github pages with gh-pages package 
