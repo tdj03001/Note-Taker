e009f2cb6dadda5a01b1060a3361798f44b13b91
tdj03001 <tyjenki@gmail.com>
2020-02-10 13:13:39 -0500

    removed old deleted files

--- Develop/public/routes/apiRoutes.js
+++
@@ -1,7 +0,0 @@
-const db = require("../../db/db.json");
-
-app.post("/api/notes", function (req, res) {
-  var newNote = req.body;
-  db.push(newNote);
-  res.json(newNote); //is this line needed????
-});
