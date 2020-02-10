9418baef9bfc89b9f763e31077968a0c071479b9
tdj03001 <tyjenki@gmail.com>
2020-02-09 22:35:12 -0500

    pulled routes out of here to reorganize into their own directory

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -7,34 +7,13 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//=========DATA=========================
-
-
-
-
+app.use(express.static('public'));
 
 
 //==========ROUTES============================
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../index.html"));
-});
 
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../notes.html"));
-});
-
-//change the route below to be a page where you see the parsed json of the notes (might be a bonus)
-app.get("/api/savednotes", function (req, res) {
-  return res.json(characters);
-});
-
-
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-// var server = http.createServer(handleRequest); //MUST PASS a function to http.createServer()
-
-
+require("../../routes/apiRoutes");
+require("../../routes/htmlRoutes");
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
