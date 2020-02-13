1af30f76c7269671b5d22e1655c00c80e6fd212a
tdj03001 <tyjenki@gmail.com>
2020-02-12 22:26:31 -0500

    fs is now writing to db.json file with POST request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
-const noteArray = require("../data/noteData.js");
+const fs = require("fs");
+const noteArray = require("../data/db.json");
+
 
 module.exports = function (app) {
 
@@ -7,10 +9,37 @@ module.exports = function (app) {
   });
 
   app.post("/api/notes", function (req, res) {
-    noteArray.push(req.body);
     res.json(true);
-  });
 
-  //add the DELETE route
-  app.delete("/api/notes/:id")
+    fs.readFile("./data/db.json", "utf-8", function (err, data) {
+      if (err) throw err;
+
+      var arrayOfObjects = JSON.parse(data);
+      arrayOfObjects.push(req.body);
+      console.log(arrayOfObjects);
+
+      fs.writeFile("./data/db.json", JSON.stringify(arrayOfObjects), "utf-8", function (err) {
+        if (err) throw err;
+        console.log("Done!");
+      })
+    })
+
+
+
+  })
+
+
+  app.delete("/api/notes/:id", function (req, res) {
+    //see below
+  })
+
 }
+
+
+// DEVELOPMENT NOTES=======================================================================
+
+// deploy to heroku, need index.php and something else (composer.json?)
+// deploy to github pages with gh-pages package 
+//add fs code to the app.delete. Write/create id on save. Use loop to loop through json file by id when deleting, and to create the id (basically the last id in the existing list +1 for the new item's id)
+
+// ===========================================================================
