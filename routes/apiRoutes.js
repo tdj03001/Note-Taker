8f9a0fd4128985d95ab6977285759040898a029d
tdj03001 <tyjenki@gmail.com>
2020-02-13 23:34:35 -0500

    final commit before submission

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const noteArray = require("../db/db.json");
 
-
 module.exports = function (app) {
 
   app.get("/api/notes", function (req, res) {
 
-    fs.readFile("./data/db.json", function (err, data) {
+    fs.readFile("./db/db.json", function (err, data) {
       if (err) return res.sendStatus(500)
       try {
         notes = JSON.parse(data)
@@ -20,59 +19,59 @@ module.exports = function (app) {
   app.post("/api/notes", function (req, res) {
     res.json(true);
 
-    fs.readFile("./data/db.json", "utf-8", function (err, data) {
+    fs.readFile("./db/db.json", "utf-8", function (err, data) {
       if (err) throw err;
 
       const arrayOfObjects = JSON.parse(data);
       arrayOfObjects.push(req.body);
       console.log(arrayOfObjects);
 
-      fs.writeFile("./data/db.json", JSON.stringify(arrayOfObjects), "utf-8", function (err) {
+      fs.writeFile("./db/db.json", JSON.stringify(arrayOfObjects), "utf-8", function (err) {
         if (err) throw err;
 
         console.log("Done!");
       })
     })
-  })
-
-  app.delete("/api/notes/:id", function (req, res) {
-    // for (const i = 0; i < noteArray.length; i++) {
-    fs.readFile("./data/db.json", "utf-8", function (err, data) {
-      if (err) throw err;
-      const arrayOfObjects = JSON.parse(data);
-      for (let i = 0; i < arrayOfObjects.length; i++) {
-        let query = { id: req.params.id }
-        if (req.params.id === id) {
-          delete query;
-        }
-      }
-
-      // console.log(noteArray[1].id)
-
-      // let note = req.body;
-      // for (let i = 0; i < noteArray.length; i++) {
-      //   if (noteArray.id === req.params.id) {
-      //     delete note;
-      //   }
-
-      //   //   // res.json(noteArray);
-      // }
-
-      // let id = parseInt(req.params.id);
-      // delete noteArray[id];
-      // res.json(noteArray);
-
-      fs.writeFile("./data/db.json", JSON.stringify(arrayOfObjects), "utf-8", function (err) {
-        if (err) throw err;
-
-        console.log("Done!");
-      })
-    })
-
-
-
   })
 };
+
+//   app.delete("/api/notes/:id", function (req, res) {
+
+//     //   // for (const i = 0; i < noteArray.length; i++) {
+//     //   fs.readFile("./data/db.json", "utf-8", function (err, data) {
+//     //     if (err) throw err;
+//     //     const arrayOfObjects = JSON.parse(data);
+//     //     for (let i = 0; i < arrayOfObjects.length; i++) {
+//     //       let query = { id: req.params.id }
+//     //       if (req.params.id === id) {
+//     //         delete query;
+//     //       }
+//     //     }
+
+//     //     // console.log(noteArray[1].id)
+
+//     //     // let note = req.body;
+//     //     // for (let i = 0; i < noteArray.length; i++) {
+//     //     //   if (noteArray.id === req.params.id) {
+//     //     //     delete note;
+//     //     //   }
+
+//     res.json(noteArray);
+//     //     // }
+
+//     //     // let id = parseInt(req.params.id);
+//     //     // delete noteArray[id];
+//     //     // res.json(noteArray);
+
+//     //     fs.writeFile("./data/db.json", JSON.stringify(arrayOfObjects), "utf-8", function (err) {
+//     //       if (err) throw err;
+
+//     //       console.log("Done!");
+//     //     })
+//     //   })
+
+//   })
+// };
 
 
 // let query = { id: req.params.id }
@@ -91,6 +90,9 @@ module.exports = function (app) {
 // fix duplicate id issue - when page is refreshed, new id increment starts over
 // get delete working
 // get heroku css working
+// get ids working
+//get delete working
+//get heroku css working
 // README
 // could deploy to github pages with gh-pages package 
 
